1318ebd5c8986fbeb79ee494dce1333d3aa54592
stacksmith <fpgasm@apple2.x10.mx>
2014-10-10 19:10:56 -0700

    added GPLv3 license for now

--- cl-fpgasm-device.lisp
+++ cl-fpgasm-device.lisp
@@ -1,5 +1,22 @@
 ;;;; cl-fpgasm-device.lisp
+#|******************************************************************************
+ Copyright 2012 Victor Yurkovsky
 
+    This file is part of cl-fpgasm
+
+    FPGAsm is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FPGAsm is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cl-fpgasm.  If not, see <http://www.gnu.org/licenses/>.
+******************************************************************************|#
 (in-package #:cl-fpgasm-device)
 
 
